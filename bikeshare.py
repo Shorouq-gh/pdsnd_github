51b5b3a7e417fc6882c2d44e5a92afd7c28ed382
Shorouq A <shorouqmm1@gmail.com>
2020-01-25 13:50:39 +0300

    Upadating user_stats() function description

--- bikeshare.py
+++ bikeshare.py
@@ -133,7 +133,7 @@ def trip_duration_stats(df):
 
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    """Displays statistics on bikeshare users. (count of user type, gender info, birth year info)"""
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
